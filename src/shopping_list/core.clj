6c3b04fd8d6d996af8e04862a1bc58ada320cb47
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-09 13:34:47 +0000

    prompt user for command

--- src/shopping_list/core.clj
+++ src/shopping_list/core.clj
@@ -6,3 +6,14 @@
 (defn add-shopping
   [shopping]
   (swap! shoppings conj shopping))
+
+(loop [you-say (read-line)]
+  (if (= you-say "quit")
+    (println "we are done")
+    (recur (do (println you-say)
+               (read-line)))))
+
+
+
+
+
