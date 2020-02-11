e75ae96c4b67f76e2698dbc505b6658b78236e57
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2020-02-11 21:04:12 +0000

    used recursion

--- src/shopping_list/core.clj
+++ src/shopping_list/core.clj
@@ -5,23 +5,11 @@
 
 (def output-file "./things-to-buy.txt")
 
-(def shoppings (atom []))
-
-(defn add-shopping
-  [shopping]
-  (swap! shoppings conj shopping))
-
 (defn prompt
   [msg]
   (println msg)
   (read-line))
 
-(defn add-product-to-shoppings
-  []
-  (let [product (prompt "What to buy?")
-        amount (prompt "How many?")]
-    (add-shopping {:product product 
-                   :amount amount})))
 
 (defn shopping->str
   [x]
@@ -40,11 +28,14 @@
 
 (defn -main
   [& args]
-  (loop [choice (prompt options)]
+  (loop [shoppings []
+         choice (prompt options)]
     (case choice
-      "1" (do (add-product-to-shoppings)
-            (recur (prompt options)))
-      "2" (do (save-shopping-list output-file @shoppings)
+      "1" (recur (conj shoppings {:product (prompt "What to buy?")
+                                  :quantity (prompt "How many?")})
+            (prompt options))
+      "2" (do (save-shopping-list output-file shoppings)
             (println "Shopping list saved"))
       (do (println "Invalid choice!!! Try again")
-        (recur (prompt options))))))
+        (recur shoppings
+          (prompt options))))))
